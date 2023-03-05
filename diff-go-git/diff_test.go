6e41f6d297aa751aedf5e4d3e05ff142500ac624
Steven Penny <rqwwof@riseup.net>
2023-03-04 18:37:40 -0600

    deleted:    diff-go-git/go.mod

--- diff-go-git/diff_test.go
+++ diff-go-git/diff_test.go
@@ -5,16 +5,6 @@ import (
    "testing"
 )
 
-/*
-utils\diff\diff.go
-
-FIND THIS
-20:func Do(src, dst string) (diffs []diffmatchpatch.Diff) {
-
-42:func Dst(diffs []diffmatchpatch.Diff) string {
-53:func Src(diffs []diffmatchpatch.Diff) string {
-*/
-
 func Test_Diff(t *testing.T) {
    repo, err := PlainOpen(`D:\GitHub\git`)
    if err != nil {
